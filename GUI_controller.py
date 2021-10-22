0e17e18b3d42736384785ee4a06b13451a89fe99
Khristian Jones <jones.khristian26@gmail.com>
2021-10-22 14:41:38 -0700

    Fleshed out GUI

--- GUI_controller.py
+++ GUI_controller.py
@@ -6,6 +6,8 @@ root = Tk()
  
 # Open window having dimension 100x100
 root.geometry('400x400')
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=2)
 
 def activatePopUp1():
     if PopUp1['text']=="Pop Up 1 Deactivated":
@@ -40,11 +42,25 @@ PopUp3 = Button(root, text = 'Pop Up 3 Deactivated', bd = '5',
                           command = activatePopUp3)
 PopUp4 = Button(root, text = 'Pop Up 4 Deactivated', bd = '5',
                           command = activatePopUp4)
+
+#cretes sliders (PWM gen)
+PopUpSlider1 = Scale(root,from_=0, to=255, orient=HORIZONTAL)
+PopUpSlider2 = Scale(root,from_=0, to=255, orient=HORIZONTAL)
+PopUpSlider3 = Scale(root,from_=0, to=255, orient=HORIZONTAL)
+PopUpSlider4 = Scale(root,from_=0, to=255, orient=HORIZONTAL)
 # Set the position of button on the top of window.  
 
 
-PopUp1.pack(side = 'top')   
-PopUp2.pack(side = 'top')
-PopUp3.pack(side = 'top')
-PopUp4.pack(side = 'top') 
+PopUp1.grid(row=0,column=0)   
+PopUpSlider1.grid(row=0,column=1)
+
+PopUp2.grid(row=1,column=0)
+PopUpSlider2.grid(row=1,column=1)
+
+PopUp3.grid(row=2,column=0)
+PopUpSlider3.grid(row=2,column=1)
+
+PopUp4.grid(row=3,column=0)
+PopUpSlider4.grid(row=3,column=1)
+
 root.mainloop()

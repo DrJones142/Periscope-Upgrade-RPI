9c8dbf80c9bd2faf91aaa9081c4f38b6f3252934
Khristian Jones <jones.khristian26@gmail.com>
2021-10-25 09:38:41 -0700

    Made changes

--- Controller_Test_v2.py
+++ Controller_Test_v2.py
@@ -17,12 +17,27 @@ def raisePopUp(PopUpNum):
     elif PopUpNum == 2:
         pin1.value = True
     elif PopUpNum == 3:
-        pin2.value == True
+        pin2.value = True
     elif PopUpNum == 4:
         pin3.value = True
     else:
         print("Invalid")
 
+def lowerPopUp(PopUpNum):
+    if PopUpNum == 1:
+        pin0.value = False
+    elif PopUpNum == 2:
+        pin1.value = False
+    elif PopUpNum == 3:
+        pin2.value = False
+    elif PopUpNum == 4:
+        pin3.value = False
+    else:
+        print("Invalid")
+
+def setPWMValue(channel,dutyCycle):
+    dutyCycle_adjusted = dutyCycle*256
+    pca.channels[channel].duty_cycle = dutyCycle_adjusted
 
 pin0 = mcp.get_pin(8)
 pin1 = mcp.get_pin(9)
@@ -34,6 +49,15 @@ pin2.switch_to_output(value=False)
 pin3.switch_to_output(value=False)
 
 pca.frequency = 1000
+
+# raisePopUp(2)
+# setPWMValue(5,255)
+# setPWMValue(6,255)
+# time.sleep(5)
+# lowerPopUp(2)
+# time.sleep(5)
+# setPWMValue(5,0)
+# setPWMValue(6,0)
 
 led_channel1 = pca.channels[0]
 
@@ -52,16 +76,16 @@ led_channel4 = pca.channels[3]
 led_channel4.duty_cycle = 0xffff
 
 led_channel5 = pca.channels[4]
-led_channel5.duty_cycle = 0x7fff
+led_channel5.duty_cycle = 0
 
 led_channel6 = pca.channels[5]
-led_channel6.duty_cycle = 0x7fff
+led_channel6.duty_cycle = 0
 
 led_channel7 = pca.channels[6]
-led_channel7.duty_cycle = 0x7fff
+led_channel7.duty_cycle = 0
 
 led_channel8 = pca.channels[7]
-led_channel8.duty_cycle = 0x7fff
+led_channel8.duty_cycle = 0
 
 
 # time.sleep(5)
--- GUI_controller.py
+++ GUI_controller.py
@@ -6,34 +6,55 @@ from tkinter import *
 root = Tk()             
  
 # Open window having dimension 100x100
-root.geometry('400x400')
+root.geometry('500x500')
 root.columnconfigure(0, weight=1)
 root.columnconfigure(1, weight=2)
 
 def activatePopUp1():
     if PopUp1['text']=="Pop Up 1 Deactivated":
         PopUp1['text']="Pop Up 1 Activated"
+        raisePopUp(1)
     else:
         PopUp1['text']="Pop Up 1 Deactivated"
+        lowerPopUp(1)
 
 def activatePopUp2():
     if PopUp2['text']=="Pop Up 2 Deactivated":
         PopUp2['text']="Pop Up 2 Activated"
+        raisePopUp(2)
     else:
         PopUp2['text']="Pop Up 2 Deactivated"
+        lowerPopUp(2)
 
 def activatePopUp3():
     if PopUp3['text']=="Pop Up 3 Deactivated":
         PopUp3['text']="Pop Up 3 Activated"
+        raisePopUp(3)
     else:
         PopUp3['text']="Pop Up 3 Deactivated"
-
+        lowerPopUp(3)
 
 def activatePopUp4():
     if PopUp4['text']=="Pop Up 4 Deactivated":
         PopUp4['text']="Pop Up 4 Activated"
+        raisePopUp(4)
     else:
         PopUp4['text']="Pop Up 4 Deactivated"
+        lowerPopUp(4)
+
+def setPWMSlider1(Slider1_value):
+    pwmValue = int(Slider1_value)
+    setPWMValue(4,pwmValue)
+
+def setPWMSlider2(Slider2_value):
+    setPWMValue(5,int(Slider2_value))
+
+def setPWMSlider3(Slider3_value):
+    setPWMValue(6,int(Slider3_value))
+
+def setPWMSlider4(Slider4_value):
+    setPWMValue(7,int(Slider4_value))        
+
 # Create a Button
 PopUp1 = Button(root, text = 'Pop Up 1 Deactivated', bd = '5',
                           command = activatePopUp1)
@@ -45,10 +66,10 @@ PopUp4 = Button(root, text = 'Pop Up 4 Deactivated', bd = '5',
                           command = activatePopUp4)
 
 #cretes sliders (PWM gen)
-PopUpSlider1 = Scale(root,from_=0, to=255, orient=HORIZONTAL)
-PopUpSlider2 = Scale(root,from_=0, to=255, orient=HORIZONTAL)
-PopUpSlider3 = Scale(root,from_=0, to=255, orient=HORIZONTAL)
-PopUpSlider4 = Scale(root,from_=0, to=255, orient=HORIZONTAL)
+PopUpSlider1 = Scale(root,from_=0, to=255, orient=HORIZONTAL,command=setPWMSlider1)
+PopUpSlider2 = Scale(root,from_=0, to=255, orient=HORIZONTAL,command=setPWMSlider2)
+PopUpSlider3 = Scale(root,from_=0, to=255, orient=HORIZONTAL,command=setPWMSlider3)
+PopUpSlider4 = Scale(root,from_=0, to=255, orient=HORIZONTAL,command=setPWMSlider4)
 # Set the position of button on the top of window.  
 
 

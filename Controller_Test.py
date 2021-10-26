8d1626c6e2155800568f89bdfda178b75a5fe7f3
DrJones142 <jones.khristian26@gmail.com>
2021-10-26 16:46:17 -0700

    Added relay i2c pins

--- Controller_Test.py
+++ Controller_Test.py
@@ -16,10 +16,22 @@ pin0 = mcp.get_pin(8)
 pin1 = mcp.get_pin(9)
 pin2 = mcp.get_pin(10)
 pin3 = mcp.get_pin(11)
+
+pin4 = mcp.get_pin(12)   #pins for relay
+pin5 = mcp.get_pin(13)
+pin6 = mcp.get_pin(14)
+pin7 = mcp.get_pin(15)
+
 pin0.switch_to_output(value=False)
 pin1.switch_to_output(value=False)
 pin2.switch_to_output(value=False)
 pin3.switch_to_output(value=False)
+
+pin4.switch_to_output(value=False)
+pin5.switch_to_output(value=False)
+pin6.switch_to_output(value=False)
+pin7.switch_to_output(value=False)
+
 
 pca.frequency = 1000
 
@@ -65,3 +77,32 @@ led_channel5.duty_cycle = 0
 led_channel6.duty_cycle = 0
 led_channel7.duty_cycle = 0
 led_channel8.duty_cycle = 0
+
+time.sleep(5)
+pin4.value = True
+time.sleep(1)
+pin5.value = True
+time.sleep(1)
+pin6.value = True
+time.sleep(1)
+pin7.value = True
+time.sleep(1)
+pin4.value = False
+time.sleep(1)
+pin5.value = False
+time.sleep(1)
+pin6.value = False
+time.sleep(1)
+pin7.value = False
+
+time.sleep(1)
+pin4.value = True
+pin5.value = True
+pin6.value = True
+pin7.value = True
+
+time.sleep(0.5)
+pin4.value = False
+pin5.value = False
+pin6.value = False
+pin7.value = False

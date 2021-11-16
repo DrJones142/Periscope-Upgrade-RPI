ee0c231f427869889ff53d559159db3c5d288b9c
DrJones142 <jones.khristian26@gmail.com>
2021-11-16 14:06:43 -0700

    Added support for MC33926 motor driver

--- Controller_Test_v2.py
+++ Controller_Test_v2.py
@@ -35,18 +35,66 @@ def lowerPopUp(PopUpNum):
     else:
         print("Invalid")
 
+def forwardMotorMike():
+    pin8.value = True
+    pin9.value = False
+
+def backwardMotorMike():
+    pin8.value = False
+    pin9.value = True
+
+def brake():
+    pin8.value = False
+    pin9.value = False
+
 def setPWMValue(channel,dutyCycle):
     dutyCycle_adjusted = dutyCycle*256
     pca.channels[channel].duty_cycle = dutyCycle_adjusted
+
+def selectRelay(relayInt):
+    if relayInt & 0x1:
+        pin4.value = True
+    else:
+        pin4.value = False
+    if relayInt & 0x2:
+        pin5.value = True
+    else:
+        pin5.value = False
+    if relayInt & 0x4:
+        pin6.value = True
+    else:
+        pin6.value = False
+    if relayInt & 0x8:
+        pin7.value = True
+    else:
+        pin7.value = False
+    print(relayInt)
 
 pin0 = mcp.get_pin(8)
 pin1 = mcp.get_pin(9)
 pin2 = mcp.get_pin(10)
 pin3 = mcp.get_pin(11)
+
+pin4 = mcp.get_pin(12)  #pins for Relay control
+pin5 = mcp.get_pin(13)
+pin6 = mcp.get_pin(14)
+pin7 = mcp.get_pin(15)
+
+pin8 = mcp.get_pin(0) #A0
+pin9 = mcp.get_pin(1) #A1
+
 pin0.switch_to_output(value=False)
 pin1.switch_to_output(value=False)
 pin2.switch_to_output(value=False)
 pin3.switch_to_output(value=False)
+
+pin4.switch_to_output(value=False)
+pin5.switch_to_output(value=False)
+pin6.switch_to_output(value=False)
+pin7.switch_to_output(value=False)
+
+pin8.switch_to_output(value=False)
+pin9.switch_to_output(value=False)
 
 pca.frequency = 1000
 
@@ -59,33 +107,33 @@ pca.frequency = 1000
 # setPWMValue(5,0)
 # setPWMValue(6,0)
 
-led_channel1 = pca.channels[0]
+# led_channel1 = pca.channels[0]
 
-led_channel1.duty_cycle = 0xffff
+# led_channel1.duty_cycle = 0xffff
 
-led_channel2 = pca.channels[1]
+# led_channel2 = pca.channels[1]
 
-led_channel2.duty_cycle = 0xffff
+# led_channel2.duty_cycle = 0xffff
 
-led_channel3 = pca.channels[2]
+# led_channel3 = pca.channels[2]
 
-led_channel3.duty_cycle = 0xffff
+# led_channel3.duty_cycle = 0xffff
 
-led_channel4 = pca.channels[3]
+# led_channel4 = pca.channels[3]
 
-led_channel4.duty_cycle = 0xffff
+# led_channel4.duty_cycle = 0xffff
 
-led_channel5 = pca.channels[4]
-led_channel5.duty_cycle = 0
+# led_channel5 = pca.channels[4]
+# led_channel5.duty_cycle = 0
 
-led_channel6 = pca.channels[5]
-led_channel6.duty_cycle = 0
+# led_channel6 = pca.channels[5]
+# led_channel6.duty_cycle = 0
 
-led_channel7 = pca.channels[6]
-led_channel7.duty_cycle = 0
+# led_channel7 = pca.channels[6]
+# led_channel7.duty_cycle = 0
 
-led_channel8 = pca.channels[7]
-led_channel8.duty_cycle = 0
+# led_channel8 = pca.channels[7]
+# led_channel8.duty_cycle = 0
 
 
 # time.sleep(5)
--- GUI_controller.py
+++ GUI_controller.py
@@ -1,15 +1,17 @@
 # import everything from tkinter module
 from tkinter import *   
-from Controller_Test_v2 import *
 
+from Controller_Test_v2 import *
 
 # create a tkinter window
 root = Tk()             
  
 # Open window having dimension 100x100
-root.geometry('400x400')
+root.geometry('600x600')
 root.columnconfigure(0, weight=1)
 root.columnconfigure(1, weight=2)
+
+var = IntVar()
 
 def activatePopUp1():
     if PopUp1['text']=="Pop Up 1 Deactivated":
@@ -43,6 +45,8 @@ def activatePopUp4():
         PopUp4['text']="Pop Up 4 Deactivated"
         lowerPopUp(4)
 
+## functions for PWM Slider (speed)
+
 def setPWMSlider1(Slider1_value):
     pwmValue = int(Slider1_value)
     setPWMValue(4,pwmValue)
@@ -54,7 +58,46 @@ def setPWMSlider3(Slider3_value):
     setPWMValue(6,int(Slider3_value))
 
 def setPWMSlider4(Slider4_value):
-    setPWMValue(7,int(Slider4_value))        
+    setPWMValue(7,int(Slider4_value))
+
+def setPWMSlider5(Slider5_value):
+    setPWMValue(8,int(Slider5_value))
+
+## functions for PWM (brightness) 
+
+def setPWMLEDSlider1(SliderLED1_value):
+    pwmValue = int(SliderLED1_value)
+    setPWMValue(0,pwmValue)
+
+def setPWMLEDSlider2(SliderLED2_value):
+    pwmValue = int(SliderLED2_value)
+    setPWMValue(1,pwmValue)
+
+def setPWMLEDSlider3(SliderLED3_value):
+    pwmValue = int(SliderLED3_value)
+    setPWMValue(2,pwmValue)
+
+def setPWMLEDSlider4(SliderLED4_value):
+    pwmValue = int(SliderLED4_value)
+    setPWMValue(3,pwmValue)
+
+       
+
+def RadioButtonSelect():
+    print("You selected option " +str(var.get()))
+    selectRelay(int(var.get()))
+
+def activateMotorMikeBackward(event):
+    print("Backward")
+    backwardMotorMike()
+
+def activateMotorMikeForwarrd(event):
+    print("Forward")
+    forwardMotorMike()
+
+def release(event):
+    print("Released")
+    brake()
 
 # Create a Button
 PopUp1 = Button(root, text = 'Pop Up 1 Deactivated', bd = '5',
@@ -66,24 +109,82 @@ PopUp3 = Button(root, text = 'Pop Up 3 Deactivated', bd = '5',
 PopUp4 = Button(root, text = 'Pop Up 4 Deactivated', bd = '5',
                           command = activatePopUp4)
 
+MotorMikeForwardButton = Button(root, text= 'Forward', bd = '5',
+                          )
+MotorMikeFBackwardButton = Button(root, text= 'Backward', bd = '5',
+                          )
+
+MotorMikeForwardButton.bind('<ButtonPress>',activateMotorMikeForwarrd) 
+MotorMikeForwardButton.bind('<ButtonRelease>',release)
+
+MotorMikeFBackwardButton.bind('<ButtonPress>',activateMotorMikeBackward)
+MotorMikeFBackwardButton.bind('<ButtonRelease>',release)
+
 #cretes sliders (PWM gen)
 PopUpSlider1 = Scale(root,from_=0, to=255, orient=HORIZONTAL,command=setPWMSlider1)
 PopUpSlider2 = Scale(root,from_=0, to=255, orient=HORIZONTAL,command=setPWMSlider2)
 PopUpSlider3 = Scale(root,from_=0, to=255, orient=HORIZONTAL,command=setPWMSlider3)
 PopUpSlider4 = Scale(root,from_=0, to=255, orient=HORIZONTAL,command=setPWMSlider4)
+MotorMikeSlider = Scale(root,from_=0, to=255, orient=HORIZONTAL,command=setPWMSlider5)
 # Set the position of button on the top of window.  
 
 
+#creates sliders for LED brightness
+PopUpLEDSlider1 = Scale(root,from_=0, to=255, orient=HORIZONTAL,command=setPWMLEDSlider1)
+PopUpLEDSlider2 = Scale(root,from_=0, to=255, orient=HORIZONTAL,command=setPWMLEDSlider2)
+PopUpLEDSlider3 = Scale(root,from_=0, to=255, orient=HORIZONTAL,command=setPWMLEDSlider3)
+PopUpLEDSlider4 = Scale(root,from_=0, to=255, orient=HORIZONTAL,command=setPWMLEDSlider4)
+
+#creates Radiobuttons for tip/tilts
+
+MotorMikeA_EL = Radiobutton(root,text="A EL",variable=var,value=0,command=RadioButtonSelect)
+MotorMikeA_AZ = Radiobutton(root,text="A AZ",variable=var,value=1,command=RadioButtonSelect)
+
+MotorMikeB_EL = Radiobutton(root,text="B EL",variable=var,value=2,command=RadioButtonSelect)
+MotorMikeB_AZ = Radiobutton(root,text="B AZ",variable=var,value=3,command=RadioButtonSelect)
+
+MotorMikeB_Rotate45 = Radiobutton(root,text="B Rot 45 (EL)",variable=var,value=4,command=RadioButtonSelect)
+MotorMikeB_Lock = Radiobutton(root,text="B Lock",variable=var,value=5,command=RadioButtonSelect)
+
+MotorMikeC_EL = Radiobutton(root,text="C EL",variable=var,value=6,command=RadioButtonSelect)
+MotorMikeC_AZ = Radiobutton(root,text="C AZ",variable=var,value=7,command=RadioButtonSelect)
+
+MotorMikeD_EL = Radiobutton(root,text="D EL",variable=var,value=8,command=RadioButtonSelect)
+MotorMikeD_AZ = Radiobutton(root,text="D AZ",variable=var,value=9,command=RadioButtonSelect)
+
 PopUp1.grid(row=0,column=0)   
 PopUpSlider1.grid(row=0,column=1)
+PopUpLEDSlider1.grid(row=0,column=2)
 
 PopUp2.grid(row=1,column=0)
 PopUpSlider2.grid(row=1,column=1)
+PopUpLEDSlider2.grid(row=1,column=2)
 
 PopUp3.grid(row=2,column=0)
 PopUpSlider3.grid(row=2,column=1)
+PopUpLEDSlider3.grid(row=2,column=2)
 
 PopUp4.grid(row=3,column=0)
 PopUpSlider4.grid(row=3,column=1)
+PopUpLEDSlider4.grid(row=3,column=2)
+
+MotorMikeA_EL.grid(row=4,column=0)
+MotorMikeA_AZ.grid(row=4,column=1)
+
+MotorMikeB_EL.grid(row=5,column=0)
+MotorMikeB_AZ.grid(row=5,column=1)
+
+MotorMikeB_Rotate45.grid(row=6,column=0)
+MotorMikeB_Lock.grid(row=6,column=1)
+
+MotorMikeC_EL.grid(row=7,column=0)
+MotorMikeC_AZ.grid(row=7,column=1)
+
+MotorMikeD_EL.grid(row=8,column=0)
+MotorMikeD_AZ.grid(row=8,column=1)
+
+MotorMikeForwardButton.grid(row=9,column=0)
+MotorMikeFBackwardButton.grid(row=9,column=1)
+MotorMikeSlider.grid(row=9,column=2)
 
 root.mainloop()
